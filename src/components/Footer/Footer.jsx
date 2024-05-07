70f0822ee8be283866b4778c7a14690bbf6c9983
MikkoKonov <kono.mih2020@gmail.com>
2024-05-07 19:37:12 +0300

    исправление бага

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import Links from '../Links/Links';
-import classes from './Footer.module.css'
+import classes from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', maxWidth: '75vw', margin: '1.5rem auto'}}>
-            <Logo/>
-            <div style={{marginLeft: '1.5rem'}}>
-                <Links name='Главная' link='/'/>
-                <Links name='Скачать APK' link='#APK'/>
-                <Links name='Telegram' link='https://t.me/Romas1337'/>
-                <Links name='Купить премиум' link='#premium'/>
-                <Links name='Обзор сервера' link='https://youtu.be/I5-nAg_Dbxg?si=QcmKZILa05mLDFK4'/>
+        <div className={classes.footerContainer}>
+            <Logo />
+            <div className={classes.linksContainer}>
+                <div className={classes.linkGroup}>
+                    <div className={classes.linkMargin}>
+                        <Links name='Скачать APK' link='#APK'/>
+                    </div>
+                    <Links name='Telegram' link='https://t.me/Romas1337'/>
+                </div>
+                <div className={classes.linkGroup}>
+                    <div className={classes.linkMargin}>
+                        <Links name='Купить премиум' link='#premium'/>
+                    </div>
+                    <Links name='Обзор сервера' link='https://youtu.be/I5-nAg_Dbxg?si=QcmKZILa05mLDFK4'/>
+                </div>
             </div>
             <div className={classes.copyright}>© Copyright AvaZoom</div>
         </div>
