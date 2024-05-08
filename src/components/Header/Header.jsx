b135331f73e03e39d380b536ce6a3f8d02e69a67
MikkoKonov <kono.mih2020@gmail.com>
2024-05-08 16:33:55 +0300

    klsdcm

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import Links from '../Links/Links';
-import classes from './Header.module.css'
+import useDeviceDetect from '../../Hooks/useDeviceDetected';
+import classes from './Header.module.css';
 import ButtonOutline from '../ButtonOutline/ButtonOutline';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const { isMobile } = useDeviceDetect();
+
     return (
-        <div style={{display:'flex', maxWidth: '75vw', justifyContent: 'space-between', margin: '1.5rem auto', alignItems: 'center'}}>
-            <Logo/>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <Links name='Главная' link='/'/>
-                <Links link='https://t.me/Romas1337' name='Telegram'/>
-                <a href='#premium'>
-                    <ButtonOutline name='Купить премиум' style={null}/>
-                </a>
-            </div>
+        <div className={classes.header}>
+            { isMobile ? (
+                <>
+                    <Logo/>
+                    <a href='https://t.me/Romas1337'>
+                        <ButtonOutline name='Telegram' style={null}/>
+                    </a>
+                </>
+            ) : (
+                <>
+                    <Logo/>
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <Links name='Главная' link='/'/>
+                        <Links link='https://t.me/Romas1337' name='Telegram'/>
+                        <a href='#premium'>
+                            <ButtonOutline name='Купить премиум' style={null}/>
+                        </a>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
